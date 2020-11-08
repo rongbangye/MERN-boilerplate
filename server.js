317315a3ece7c26f96f71c991c4df7a4af0d8413
rongbang <rongbang.ye@gmail.com>
2020-11-07 21:36:19 -0800

    move private info to env file

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+// dotenv.config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.error(err));
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Register
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
